6117c9b8bf4ee1f96e0122bdbf2eb4bd13972c09
SornDevT <sorndev.teach@gmail.com>
2025-07-31 02:07:10 +0700

    upload project to git VDO 02

---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App MiniPos 25</title>
+
+
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+
+    <h1>Laravel App</h1>
+
+    <hr>
+
+    <div id="app"></div>
+    
+
+    @vite('resources/js/app.js')
+</body>
+</html>
